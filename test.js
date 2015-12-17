b70e1c6a414cbbb8eeb460e4314ca24199c17b87
Matt Paul <mattpaul001@icloud.com>
2015-12-17 12:23:35 +0000

    Prepull yana

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
-var connectedLink1 = "http://serket.uk/badges/badgelist-format";
+$(document).ready(function() {
+
+
+// This will get the first returned node in the jQuery collection.
+var myNewChart = new Chart(ctx);
+
 
 $.getJSON(connectedLink1, function showData(data){
 
@@ -24,5 +29,59 @@ $.getJSON(connectedLink2, function showData(data){
       console.log(data[i].dispenseDate.substr(0,7))
     };
   };
+
+  var options = {
+
+    ///Boolean - Whether grid lines are shown across the chart
+    scaleShowGridLines : true,
+
+    //String - Colour of the grid lines
+    scaleGridLineColor : "rgba(0,0,0,.05)",
+
+    //Number - Width of the grid lines
+    scaleGridLineWidth : 1,
+
+    //Boolean - Whether to show horizontal lines (except X axis)
+    scaleShowHorizontalLines: true,
+
+    //Boolean - Whether to show vertical lines (except Y axis)
+    scaleShowVerticalLines: true,
+
+    //Boolean - Whether the line is curved between points
+    bezierCurve : true,
+
+    //Number - Tension of the bezier curve between points
+    bezierCurveTension : 0.4,
+
+    //Boolean - Whether to show a dot for each point
+    pointDot : true,
+
+    //Number - Radius of each point dot in pixels
+    pointDotRadius : 4,
+
+    //Number - Pixel width of point dot stroke
+    pointDotStrokeWidth : 1,
+
+    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+    pointHitDetectionRadius : 20,
+
+    //Boolean - Whether to show a stroke for datasets
+    datasetStroke : true,
+
+    //Number - Pixel width of dataset stroke
+    datasetStrokeWidth : 2,
+
+    //Boolean - Whether to fill the dataset with a colour
+    datasetFill : true,
+
+    //String - A legend template
+    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+};
+
+  var connectedLink1 = "http://serket.uk/badges/badgelist-format";
+  var ctx = $("#myChart").get(0).getContext("2d");
+
+});
 
 });
