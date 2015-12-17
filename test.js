9737467088d51870aee15eed3e23148b1597fb16
Proskurina <yproskurina@yahoo.com>
2015-12-17 12:23:00 +0000

    Chart. First attempt

--- test.js
+++ test.js
@@ -1,29 +1,98 @@
-var connectedLink1 = "http://serket.uk/badges/badgelist-format";
+$( document ).ready(function() {
 
-$.getJSON(connectedLink1, function showData(data){
+  var connectedLink = "http://serket.uk/badges/badgelist"
 
-  var data0 = data.data[610]
-  var data1 = data.data[739]
+  $.getJSON(connectedLink, function showData(data){
 
+    var dates = [0,0,0,0]
 
-  console.log(data0)
-  console.log(data1)
-
-});
-
-var connectedLink2 = "http://serket.uk/badges/badgelist"
-
-$.getJSON(connectedLink2, function showData(data){
-
-  var data0 = data[610]
-  var data1 = data[739]
-
-
-  for (var i=0; i<data.length; i++){
-    if (data[i].dispenseDate){
-      console.log(data[i].dispenseDate.substr(0,7))
+    for (var i=0; i<data.length; i++){
+      (function (i){
+        if (data[i].dispenseDate){
+          console.log (data[i].dispenseDate.substr(5,2))
+          if (data[i].dispenseDate.substr(5,2) === "09"){
+            dates[0]+=1
+          } else if (data[i].dispenseDate.substr(5,2) === "10"){
+            dates[1]+=1
+          } else if (data[i].dispenseDate.substr(5,2) === "11"){
+            dates[2]+=1
+          } else if (data[i].dispenseDate.substr(5,2) === "12"){
+            dates[3]+=1;
+          };
+        };
+      })(i);
     };
-  };
+    
+    console.log(dates)
 
+    var data2 = {
+      labels: ["September", "October", "November", "December"],
+      datasets: [
+          {
+              label: "My First dataset",
+              fillColor: "rgba(220,220,220,0.2)",
+              strokeColor: "rgba(220,220,220,1)",
+              pointColor: "rgba(220,220,220,1)",
+              pointStrokeColor: "#fff",
+              pointHighlightFill: "#fff",
+              pointHighlightStroke: "rgba(220,220,220,1)",
+              data: dates
+          },
+      ]
+    };
+
+    var options = {
+
+      ///Boolean - Whether grid lines are shown across the chart
+      scaleShowGridLines : true,
+
+      //String - Colour of the grid lines
+      scaleGridLineColor : "rgba(0,0,0,.05)",
+
+      //Number - Width of the grid lines
+      scaleGridLineWidth : 1,
+
+      //Boolean - Whether to show horizontal lines (except X axis)
+      scaleShowHorizontalLines: true,
+
+      //Boolean - Whether to show vertical lines (except Y axis)
+      scaleShowVerticalLines: true,
+
+      //Boolean - Whether the line is curved between points
+      bezierCurve : true,
+
+      //Number - Tension of the bezier curve between points
+      bezierCurveTension : 0.4,
+
+      //Boolean - Whether to show a dot for each point
+      pointDot : true,
+
+      //Number - Radius of each point dot in pixels
+      pointDotRadius : 4,
+
+      //Number - Pixel width of point dot stroke
+      pointDotStrokeWidth : 1,
+
+      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+      pointHitDetectionRadius : 20,
+
+      //Boolean - Whether to show a stroke for datasets
+      datasetStroke : true,
+
+      //Number - Pixel width of dataset stroke
+      datasetStrokeWidth : 2,
+
+      //Boolean - Whether to fill the dataset with a colour
+      datasetFill : true,
+
+      //String - A legend template
+      legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+    };
+
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myLineChart = new Chart(ctx).Line(data2, options);
+
+  });
 
 });
