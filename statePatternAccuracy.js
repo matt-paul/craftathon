dbf4b0f5ecd853218706ec5f3d3a9bc7b258decc
Matt Paul <mattpaul001@icloud.com>
2015-12-18 11:21:49 +0000

    StatePatternAccuracy chart refactored into seperate file

---
+++ statePatternAccuracy.js
@@ -0,0 +1,113 @@
+// State Pattern Accuracy
+
+$( document ).ready(function() {
+
+  var connectedLink = "http://serket.uk/badges/badgelist"
+
+  $.getJSON(connectedLink, function showData(data){
+
+    var dates = [0,0,0,0,0,0]
+    var datesTrue = [0,0,0,0,0,0]
+    var month;
+
+    for (var i=0; i<data.length; i++){
+      if (data[i].dispenseDate){
+        month = data[i].dispenseDate.substr(5,2)
+        if (month === "08"){
+          dates[0]+=1
+        } else if (month === "09"){
+          dates[1]+=1
+        } else if (month === "10"){
+          dates[2]+=1
+        } else if (month === "11"){
+          dates[3]+=1
+        } else if (month === "12"){
+          dates[4]+=1
+        } else if (month === "13"){
+          dates[5]+=1;
+        };
+      };
+    };
+
+
+    for (var i=0; i<data.length; i++){
+      if (data[i].dispenseDate && data[i].statePattern){
+        month = data[i].dispenseDate.substr(5,2)
+        if (month === "08"){
+          datesTrue[0]+=1
+        } else if (month === "09"){
+          datesTrue[1]+=1
+        } else if (month === "10"){
+          datesTrue[2]+=1
+        } else if (month === "11"){
+          datesTrue[3]+=1
+        } else if (month === "12"){
+          datesTrue[4]+=1
+        } else if (month === "13"){
+          datesTrue[5]+=1;
+        };
+      };
+    };
+
+    var percentage = [];
+
+    for (var i=0; i<dates.length; i++){
+      percentage[i] = Math.round(datesTrue[i]*100/dates[i])
+    };
+
+    console.log(percentage);
+
+    var data2 = {
+      labels: ["August", "September", "October", "November", "December", "January"],
+      datasets: [
+          {
+              label: "My First dataset",
+              fillColor: "rgba(220,220,220,0.2)",
+              strokeColor: "rgba(220,220,220,1)",
+              pointColor: "rgba(220,220,220,1)",
+              pointStrokeColor: "#fff",
+              pointHighlightFill: "#fff",
+              pointHighlightStroke: "rgba(220,220,220,1)",
+              data: dates
+          },
+          {
+              label: "My First dataset",
+              fillColor: "rgba(100,220,220,0.2)",
+              strokeColor: "rgba(220,220,220,1)",
+              pointColor: "rgba(220,220,220,1)",
+              pointStrokeColor: "#fff",
+              pointHighlightFill: "#fff",
+              pointHighlightStroke: "rgba(220,220,220,1)",
+              data: datesTrue
+          },
+      ]
+    };
+
+
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myLineChart = new Chart(ctx).Bar(data2, {
+      showTooltips: false,
+      onAnimationComplete: function () {
+
+        var ctx = this.chart.ctx;
+        ctx.font = this.scale.font;
+        ctx.fillStyle = this.scale.textColor
+        ctx.textAlign = "center";
+        ctx.textBaseline = "bottom";
+
+        ourBars = this.datasets[1].bars
+
+        this.datasets[1].bars.forEach(function (bar) {
+
+          ctx.fillText(percentage[ourBars.indexOf(bar)], bar.x, bar.y - 5);
+
+        });
+
+      }
+    });
+
+
+
+      });
+
+    });
